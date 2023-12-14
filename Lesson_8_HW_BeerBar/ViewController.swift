2c31f473f7ae5cfe3c5c1f22e18847ea60343520
Aliaksandr Liakh <151917664+laviosapps@users.noreply.github.com>
2023-12-14 10:11:59 +0200

    Initial Commit

---
+++ Lesson_8_HW_BeerBar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson_8_HW_BeerBar
+//
+//  Created by Aliaksandr Liakh on 14.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
